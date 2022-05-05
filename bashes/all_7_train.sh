561814b40d33f853aeb93f1113a301508fd45274
Mandi Zhao <mandi.zhao@berkeley.edu>
2022-05-04 22:00:37 -0700

    Update all_7_train.sh

--- bashes/all_7_train.sh
+++ bashes/all_7_train.sh
@@ -2,10 +2,7 @@
 python train.py use_all_tasks=True exp_name=7Task-TempContra set_same_n=1 bsize=61 vsize=61 \
     attn.n_attn_layers=2 attn.attn_ff=128 actions.n_layers=2 actions.out_dim=64 actions.hidden_dim=128 actions.n_mixtures=7 
 
-# python train.py use_all_tasks=True exp_name=7Task-TempContra set_same_n=1 bsize=61 vsize=61 attn.n_attn_layers=2 attn.attn_ff=256 actions.n_layers=2 actions.out_dim=64 actions.hidden_dim=128 actions.n_mixtures=7; 
-# python train.py use_all_tasks=True exp_name=7Task-TempContra set_same_n=1 bsize=61 vsize=61 attn.n_attn_layers=2 attn.attn_ff=256 actions.n_layers=2 actions.out_dim=64 actions.hidden_dim=128 actions.n_mixtures=14
 
-# pabti3
 # python train.py use_all_tasks=True epochs=10 exp_name=7Task-TempContra set_same_n=1 bsize=61 vsize=61 \
 #     attn.n_attn_layers=2 attn.attn_ff=256 \
 #      actions.n_layers=2 actions.out_dim=64 actions.hidden_dim=128 actions.n_mixtures=14 \
@@ -16,8 +13,7 @@ python train.py use_all_tasks=True epochs=10 exp_name=7Task-TempContra set_same_
      actions.n_layers=2 actions.out_dim=64 actions.hidden_dim=128 actions.n_mixtures=14 \
       simclr.hidden_dim=256 simclr.compressor_dim=256  
 
-
-# pabamd3
+ 
 taskset -c 100-150 python train.py use_all_tasks=True epochs=10 exp_name=7Task-TempContra set_same_n=1 bsize=61 vsize=61 \
     attn.n_attn_layers=2 attn.attn_ff=256 \
      actions.n_layers=2 actions.out_dim=64 actions.hidden_dim=128 actions.n_mixtures=7 \
